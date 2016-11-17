c38d83c7e10a6e0538620f1217512a1441d5ba08
J David Smith <emallson@atlanis.net>
2016-11-17 13:43:39 -0500

    Basic timeseries plot figured out.
    Problem: fixed size. Need to make it responsive somehow.

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@
 var mqtt = require('mqtt');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {LineChart, XAxis, YAxis, Tooltip, Legend, Line, CartesianGrid, ResponsiveContainer} = require('recharts');
+// var {LineChart, XAxis, YAxis, Tooltip, Legend, Line, CartesianGrid, ResponsiveContainer} = require('recharts');
+var {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries} = require('react-vis');
+var dateformat = require('dateformat');
 
 class Subscription extends React.PureComponent {
   render() {
@@ -30,7 +32,7 @@ class TemperaturePlot extends React.PureComponent {
     this.props.client.on('message', (topic, message) => {
       if(topic == this.props.topic) {
         this.setState((prevState, props) => {
-          return {data: prevState.data.concat([{timestamp: Date.now(), value: message}])};
+          return {data: prevState.data.concat([{timestamp: Date.now(), value: Number.parseInt(message.toString())}]).slice(-10)};
         });
       }
     });
@@ -39,16 +41,23 @@ class TemperaturePlot extends React.PureComponent {
   render() {
     if(this.state.data.length > 0) {
       return (
-        <ResponsiveContainer>
-          <LineChart data={this.state.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-            <XAxis dataKey="timestamp"/>
-            <YAxis domain={['dataMin - 5', 'dataMax + 5']}/>
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip/>
-            <Legend/>
-            <Line type="monotone" dataKey="value" stroke="#333" />
-          </LineChart>
-        </ResponsiveContainer>
+        // <ResponsiveContainer>
+        //   <LineChart data={this.state.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        //     <XAxis dataKey="timestamp"/>
+        //     <YAxis domain={['dataMin - 5', 'dataMax + 5']}/>
+        //     <CartesianGrid strokeDasharray="3 3" />
+        //     <Tooltip separator=': '/>
+        //     <Legend/>
+        //     <Line unit="F" name="Temperature" type="monotone" dataKey="value" stroke="#333" />
+        //   </LineChart>
+        // </ResponsiveContainer>
+        <XYPlot width={300} height={300} xType="time" yDomain={[0,100]}>
+          <HorizontalGridLines/>
+          <VerticalGridLines/>
+          <XAxis title="Time"/>
+          <YAxis title="Temperature"/>
+          <LineSeries data={this.state.data.map(({timestamp, value}) => {return {x: timestamp, y: value};})}/>
+        </XYPlot>
       );
     }
     return null;
@@ -64,17 +73,3 @@ client.on('connect', () => {
 });
 
 client.subscribe('home/bedroom/temperature', () => console.log(arguments));
-client.on('message', (topic, message) => console.log(arguments));
-
-window.client = client;
-
-// var client = new Paho.MQTT.Client("127.0.0.1", 1884, "dashboard");
-// client.onConnectionLost = (err) => console.error(err);
-// client.onMessageArrived = (msg) => console.log(msg);
-
-// client.connect({onSuccess: () => {
-//   ReactDOM.render(
-//     <TemperaturePlot topic="home/bedroom/temperature" client={client} />,
-//     document.getElementById("root")
-//   )
-// }});
