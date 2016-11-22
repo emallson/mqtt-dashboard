370ac7c1a9b9e2e5bc2afc553024a2998ac66a30
J David Smith <emallson@atlanis.net>
2016-11-22 13:50:38 -0500

    Added some localstorage goodness.

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries, DiscreteColorLegend} = require('react-vis');
 var Immutable = require('immutable');
-var dateformat = require('dateformat');
+var moment = require('moment');
 
 function topic_match(pattern, topic) {
   let re = new RegExp(pattern.replace(/\+/g, '([^/]+)').replace(/#/g, '(.+)'));
@@ -43,20 +43,43 @@ function concatMerger(a, b) {
   return b;
 }
 
+function store(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch(e) {
+    console.log("Failed to preserve data in " + key + ": " + e);
+  }
+}
+
+function immutable_load(key) {
+  if(!key) { return null; }
+  let val = localStorage.getItem(key);
+  if(val == null) {
+    return null;
+  } else {
+    return JSON.parse(val);
+  }
+}
+
+function truncate_timeseries(data) {
+  let limit = moment().subtract(1, 'd');
+  return data.map((segment) => segment.map((bld) => bld.map((room) => room.filter((obj) => moment(obj.get('timestamp')).isSameOrAfter(limit)))))
+}
+
 class TemperaturePlots extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {data:  Immutable.fromJS({ shown: {}, hidden: {} })};
+    this.state = load(this.props.preserve) || {data:  Immutable.fromJS({ shown: {}, hidden: {} })};
 
     this.props.client.subscribe(this.props.topic); // false generality
     this.props.client.on('message', (topic, message) => {
       let [whole, building, room] = topic_match('+/+/temperature', topic);
 
-      this.setState((prev, props) => {return {data: prev.data.mergeWith(concatMerger, {
+      this.setState((prev, props) => {return {data: truncate_timeseries(prev. data. mergeWith (concatMerger, {
         [prev.data.get('hidden').has(building)? 'hidden' : 'shown']: {
           [building]: {[room]: [parse_temperature_msg (message)]}
         }
-      })}});
+      }))}});
     });
   }
 
@@ -64,12 +87,18 @@ class TemperaturePlots extends React.PureComponent {
     var plots = this.state.data.get('shown').map((state, building) => <TemperaturePlot key={building} data={state} />);
     return (<div>{plots.toArray()}</div>);
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.hasOwnProperty('preserve')) {
+      store(prevProps.preserve, this.state);
+    }
+  }
 }
 
 var client = mqtt.connect("ws://localhost:1884");
 client.on('connect', () => {
   ReactDOM.render(
-    <TemperaturePlots topic="+/+/temperature" client={client} />,
+    <TemperaturePlots topic="+/+/temperature" client={client} preserve="temp-data" />,
     document.getElementById("root")
   )
 });
