9c75b62d4dd0bfa42bd83f6d17847af454a3d656
J David Smith <emallson@atlanis.net>
2016-11-26 17:24:56 -0500

    Added some fancy-looking device descriptors.
    They don't pull data from MQTT yet -- working on that.

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ var ReactDOM = require('react-dom');
 var {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries, DiscreteColorLegend} = require('react-vis');
 var Immutable = require('immutable');
 var moment = require('moment');
+var ch = require('color-hash');
 
 function topic_match(pattern, topic) {
   let re = new RegExp(pattern.replace(/\+/g, '([^/]+)').replace(/#/g, '(.+)'));
@@ -51,7 +52,7 @@ function store(key, value) {
   }
 }
 
-function immutable_load(key) {
+function load(key) {
   if(!key) { return null; }
   let val = localStorage.getItem(key);
   if(val == null) {
@@ -69,7 +70,8 @@ function truncate_timeseries(data) {
 class TemperaturePlots extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = load(this.props.preserve) || {data:  Immutable.fromJS({ shown: {}, hidden: {} })};
+    this.state = load(this.props.preserve) || {data:  { shown: {}, hidden: {} }};
+    this.state.data = Immutable.fromJS(this.state.data);
 
     this.props.client.subscribe(this.props.topic); // false generality
     this.props.client.on('message', (topic, message) => {
@@ -95,10 +97,72 @@ class TemperaturePlots extends React.PureComponent {
   }
 }
 
+var hasher = new ch({saturation: 0.25, lightness: 0.6});
+class DeviceBlock extends React.PureComponent {
+  render() {
+    var subs = null;
+    if(this.props.device.subscribes.length > 0) {
+      subs = <div className="subscriptions">
+        {this.props.device.subscribes.map((topic) => <code key={topic}>{topic}</code>)}
+      </div>;
+    }
+    var pubs = null;
+    if(this.props.device.publishes.length > 0) {
+      pubs = <div className="publishes">
+        {this.props.device.publishes.map((topic) => <code key={topic}>{topic}</code>)}
+      </div>;
+    }
+    return (
+      <div className="device-block" style={{borderColor: hasher.hex(this.props.device.type)}}>
+        <div className="title" style={{backgroundColor: hasher.hex(this.props.device.type)}}>
+          <h3>{this.props.device.type}</h3>
+        </div>
+        <div className="body">
+          {this.props.device.description}
+        </div>
+        {pubs}
+        {subs}
+      </div>
+    );
+  }
+}
+
+class DeviceDescriptionRegion extends React.PureComponent {
+  render() {
+    return (
+      <div className="device-block-region">{Object.keys(this.props.devices).map((key) => <DeviceBlock key={key} device={this.props.devices[key]}/>)}</div>
+    );
+  }
+}
+
+var devices = {
+  "abcd": {
+    "type": "button",
+    "publishes": [
+      "#/force-update"
+    ],
+    "subscribes": [],
+    "description": "A simple publish-only button that forces any subscribed sensors to send updated data."
+  },
+  "efgh": {
+    "type": "temperature",
+    "publishes": [
+      "home/kitchen/temperature"
+    ],
+    "subscribes": [
+      "home/kitchen/temperature/force-update"
+    ],
+    "description": "A simple temperature sensor. Publishes temperature readings periodically, or when a subscribed message is received."
+  }
+ }
+
 var client = mqtt.connect("ws://localhost:1884");
 client.on('connect', () => {
   ReactDOM.render(
-    <TemperaturePlots topic="+/+/temperature" client={client} preserve="temp-data" />,
+    <div>
+      <div><DeviceDescriptionRegion devices={devices} /></div>
+      <div><TemperaturePlots topic="+/+/temperature" client={client} preserve="temp-data" /></div>
+    </div>,
     document.getElementById("root")
   )
 });
